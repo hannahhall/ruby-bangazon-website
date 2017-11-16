22d4ecfd72ca85959319223a5a45bc7309ade4bc
Hannah Hall <hannahkhall13@gmail.com>
2017-11-16 12:25:53 -0600

    product type scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :product_types
   get 'welcome/index'
   root 'welcome#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema.define(version: 20171114123848) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "address"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
